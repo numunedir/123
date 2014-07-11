ba0b1a6b9c17173d86148e736d23be265edd9fda
numunedir <noreply>
2014-07-11 03:46:14 +0000

    Add component trying

---
+++ ui/trying.reel/trying.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/trying.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Trying
+ * @extends Component
+ */
+exports.Trying = Component.specialize(/** @lends Trying# */ {
+    constructor: {
+        value: function Trying() {
+            this.super();
+        }
+    }
+});
